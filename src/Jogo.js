23a47b09f9dbe9904e0d00b231b551221dc5eb35
Kaiena <kaienafmartins@gmail.com>
2022-11-24 10:34:43 -0300

    add image data-test

--- src/Jogo.js
+++ src/Jogo.js
@@ -4,8 +4,11 @@ export default function Jogo(props) {
   return (
     <>
       <Photo>
-        <img src={props.images[0]} alt="" />
+        <img data-test="game-image" src={props.images[0]} alt="" />
       </Photo>
+      <ChooseWordBtn>
+        Escolher Palavra
+      </ChooseWordBtn>
     </>
   );
 }
@@ -17,4 +20,20 @@ const Photo = styled.div`
   display: flex;
   position: relative;
   right: 180px;
+`;
+
+const ChooseWordBtn = styled.div`
+  display: block;
+  position: absolute;
+  top: 140px;
+  right: -35px;
+  width: 180px;
+  height: 50px;
+  background-color: #58AF61;
+  color: #fafafa;
+  border-radius: 7px;
+  line-height: 50px;
+  font-size: 19px;
+  font-weight: 700;
+  cursor: pointer;
 `;
