6228ab1e48639521e9d7c297ffc23d1195dd5028
Kaiena <kaienafmartins@gmail.com>
2022-11-24 09:16:37 -0300

    add letters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+// import forca0 from "./images/forca0.png"
+// import forca1 from "./images/forca1.png"
+// import forca2 from "./images/forca2.png"
+// import forca3 from "./images/forca3.png"
+// import forca4 from "./images/forca4.png"
+// import forca5 from "./images/forca5.png"
+// import forca6 from "./images/forca6.png"
+import Letras from "./Letras";
 
 function App() {
+  const alphabet = [
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+  ]
+  // const images = [
+  //   forca0,
+  //   forca1,
+  //   forca2,
+  //   forca3,
+  //   forca4,
+  //   forca5,
+  //   forca6
+  // ]
   return (
     <>
-    
+      <Letras alphabet={alphabet}/>
     </>
   );
 }
---
+++ src/Letras.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export default function Letras(props) {
+  let {alphabet} = props
+  return (
+    <AlphabetWrapper>
+      {alphabet.map((letter) => (
+        <ButtonLetters
+        >
+          {letter}
+        </ButtonLetters>
+      ))}
+    </AlphabetWrapper>
+  );
+}
+
+const AlphabetWrapper = styled.div`
+  display: flex;
+  position: relative;
+  top: 360px;
+  flex-wrap: wrap;
+  width: 650px;
+`;
+
+const ButtonLetters = styled.button`
+  border-radius: 8px;
+  border: none;
+  margin: 5px;
+  width: 40px;
+  height: 40px;
+  font-size: 15px;
+  background-color: #7aa7c7;
+  color: #39739d;
+  text-transform: uppercase;
+  font-weight: 700;
+  &:hover {
+    cursor: pointer;
+  }
+`;
